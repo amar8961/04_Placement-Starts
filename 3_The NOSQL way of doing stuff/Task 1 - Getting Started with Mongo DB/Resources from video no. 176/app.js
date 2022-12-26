aefe0c7204ae9f5773524eb8ac0eb80964cc2bca
amar8961 <amarkumar8961@gmail.com>
2022-12-27 01:06:14 +0530

    Add to Cart & Displaying the Cart Items

--- 3_The NOSQL way of doing stuff/Task 1 - Getting Started with Mongo DB/Resources from video no. 176/app.js
+++ 3_The NOSQL way of doing stuff/Task 1 - Getting Started with Mongo DB/Resources from video no. 176/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('63a99effa761c68875d78d77')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
