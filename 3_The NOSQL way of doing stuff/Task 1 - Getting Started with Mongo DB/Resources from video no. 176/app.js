82e4bcf2dc0178c4f80c52b094d31e343d017669
amar8961 <amarkumar8961@gmail.com>
2022-12-27 12:45:58 +0530

    Create Product Schema & Save Data Through Mongoose

--- 3_The NOSQL way of doing stuff/Task 1 - Getting Started with Mongo DB/Resources from video no. 176/app.js
+++ 3_The NOSQL way of doing stuff/Task 1 - Getting Started with Mongo DB/Resources from video no. 176/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,23 +18,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('63a99effa761c68875d78d77')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('63a99effa761c68875d78d77')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+var DB_Name = 'shop'
 mongoose
   .connect(
-    'mongodb+srv://amar:amar4456@cluster0.qwphzua.mongodb.net/?retryWrites=true&w=majority'
+    `mongodb+srv://amar:amar4456@cluster0.qwphzua.mongodb.net/${DB_Name}?retryWrites=true&w=majority`
   )
   .then(result => {
     app.listen(3000);
